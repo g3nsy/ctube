a78aea3a912291cd7eae4bd864402087037cbb4c
g3nsy <g3nsydev@gmail.com>
2024-05-23 20:06:43 +0200

    Improved Command class structure. Argument and Option classes Introduced to allow better future development of the program.

--- ctube/cmds.py
+++ ctube/cmds.py
@@ -1,92 +1,127 @@
 from enum import Enum
 from dataclasses import dataclass
-from typing import List, Set, Optional
-from ctube.colors import get_color_names
+from typing import Optional, Type, Set
 
 
-@dataclass
-class Command:
+@dataclass(unsafe_hash=True)
+class Argument:
     name: str
     description: str
-    expected_args: str
-    expected_num_args: int
-    accepted_args: Optional[Set[str]] = None
+    type: Type
+
+
+@dataclass(unsafe_hash=True)
+class Option:
+    short_name: str
+    long_name: str
+    required_args: int
+    description: Optional[str] = None
+    accepted_args: Optional[Set[Argument]] = None
+    args: Optional[Set[Argument]] = None
+
+
+@dataclass(unsafe_hash=True)
+class Command:
+    name: str
+    short_description: str
+    long_description: str
+    required_args: int
+    opts: Optional[Set[Option]] = None
+    accepted_args: Optional[Set[Argument]] = None
+    args: Optional[Set[Argument]] = None
 
 
 class Commands(Enum):
     SEARCH = Command(
         name="search", 
-        description="Search by artist name",
-        expected_args="<artist name>",
-        expected_num_args=1
+        short_description="",
+        long_description="",
+        required_args=1,
+        args={
+            Argument(
+                name="<artist name>", 
+                description="", 
+                type=str
+            )
+        }
     )
+
     ID = Command(
         name="id", 
-        description="Search by artist ID",
-        expected_args="<artist id>",
-        expected_num_args=1
+        short_description="",
+        long_description="",
+        required_args=1,
+        args={
+            Argument(
+                name="<artist id>", 
+                description="", 
+                type=str
+            )
+        }
     )
+
     DOWNLOAD = Command(
         name="download", 
-        description="Downloads",
-        expected_args="Indexes",
-        expected_num_args=-1
+        short_description="",
+        long_description="",
+        required_args=1,
+        args={
+            Argument(
+                name="<indexes>", 
+                description="", 
+                type=str
+            )
+        }
     )
+
     EXIT = Command(
         name="exit", 
-        description="Exit the program",
-        expected_args="",
-        expected_num_args=0
+        short_description="",
+        long_description="",
+        required_args=0
     )
+
     HELP = Command(
         name="help", 
-        description="Print the help message",
-        expected_args="",
-        expected_num_args=0
+        short_description="",
+        long_description="",
+        required_args=0,
+        opts={
+            Option(
+                short_name="-v", 
+                long_name="--verbose", 
+                description="",
+                required_args=0
+            )
+        }
     )
+
     CLEAR = Command(
         name="clear", 
-        description="Clear the console",
-        expected_args="",
-        expected_num_args=0
+        short_description="",
+        long_description="",
+        required_args=0,
     )
+
     INFO = Command(
         name="info", 
-        description="Print the info of a specific command",
-        expected_args="<command name>",
-        expected_num_args=1,
-        accepted_args={"search", "id", "exit", "help", "clear", "info"}
-    )
-    PROMPT_CHAR = Command(
-        name="prompt-char",
-        description="Change the prompt char",
-        expected_args="<char>",
-        expected_num_args=1,
-    )
-    PROMPT_COLOR = Command(
-        name="prompt-color",
-        description="Change the prompt color",
-        expected_args="<color>",
-        expected_num_args=1,
-        accepted_args=get_color_names()
+        short_description="",
+        long_description="",
+        required_args=1,
+        accepted_args={
+            Argument(name="search", description="The 'search' command", type=str),
+            Argument(name="id", description="The 'id' command", type=str),
+            Argument(name="exit", description="The 'exit' command", type=str),
+            Argument(name="help", description="The 'help' command", type=str),
+            Argument(name="clear", description="The 'clear' command", type=str),
+            Argument(name="info", description="The 'info' command", type=str)
+        }
     )
 
 
-def get_cmd_with_args() -> List[Commands]:
-    return [
-        cmd for cmd in Commands 
-        if cmd.value.expected_num_args > 0 
-        or cmd.value.expected_num_args == -1
-    ]
-
-
-def get_available_cmds_names() -> List[str]:
-    return [cmd.value.name for cmd in Commands]
+def get_cmd_with_args() -> Set[Commands]:
+    return {cmd for cmd in Commands if cmd.value.required_args > 0}
 
 
 def get_cmd_by_name(cmd_name: str) -> Commands:
     return Commands.__members__[cmd_name.replace("-", "_").upper()]
-
-
-def is_command(cmd_name: str, cmd: Commands) -> bool:
-    return cmd_name == cmd.value.name
