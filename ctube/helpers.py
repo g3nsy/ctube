12c676aeff700502c52be81397576b8e44e9cf7b
g3nsy <g3nsydev@gmail.com>
2024-05-24 16:02:27 +0200

    Improved error messages

--- ctube/helpers.py
+++ ctube/helpers.py
@@ -1,6 +1,5 @@
 from typing import Tuple, List, Optional, Union
 from ctube.decorators import handle_invalid_index_syntax
-from ctube.colors import color, Color
 from ctube.containers import MusicItem
 from ctube.errors import InvalidIndexSyntax
 
@@ -21,10 +20,10 @@ def get_filtered_music_items(music_items: List[MusicItem], user_input: str) -> O
     elif isinstance(selected_indexes, slice):
         selected_music_items = music_items[selected_indexes]
         if not selected_music_items:
-            if selected_indexes.start == selected_indexes.stop + 1:
-                print(color("Invalid index", Color.RED))
+            if selected_indexes.start == selected_indexes.stop - 1:
+                raise InvalidIndexSyntax(f"Invalid index: {selected_indexes.start}")
             else:
-                print(color("Invalid slice", Color.RED))
+                raise InvalidIndexSyntax(f"Invalid slice: {selected_indexes.start}:{selected_indexes.stop}")
         else:
             return selected_music_items
     else:
@@ -33,7 +32,11 @@ def get_filtered_music_items(music_items: List[MusicItem], user_input: str) -> O
             if index >= len(music_items):
                 incorrect_indexes.append(index)
         if incorrect_indexes:
-            print(f"Invalid indexes: {', '.join(map(str, incorrect_indexes))}")
+            str_incorrect_indexes = ', '.join(map(str, incorrect_indexes))
+            if len(incorrect_indexes) > 1:
+                raise InvalidIndexSyntax(f"Invalid indexes: {str_incorrect_indexes}")
+            else:
+                raise InvalidIndexSyntax(f"Invalid index: {str_incorrect_indexes}")
         else:
             return [music_items[index] for index in selected_indexes]
 
@@ -41,7 +44,6 @@ def get_filtered_music_items(music_items: List[MusicItem], user_input: str) -> O
 def get_selected_indexes(string: str) -> Optional[Union[slice, List[int]]]:
     if string.lower() == "all":
         return None
-
     else:
         # x
         if string.isdigit():
@@ -51,17 +53,15 @@ def get_selected_indexes(string: str) -> Optional[Union[slice, List[int]]]:
         double_points_splitted_string = string.split(":")
         if len(double_points_splitted_string) > 1:
             if len(double_points_splitted_string) != 2:
-                raise InvalidIndexSyntax
+                raise InvalidIndexSyntax("Missing stop index.")
 
             first, second = double_points_splitted_string
-            
             if not first.isdigit() or not second.isdigit():
-                raise InvalidIndexSyntax
+                raise InvalidIndexSyntax("Indexes must be integers.")
 
             first, second = int(first), int(second)
-
             if first < 0 or second < 0 or second <= first:
-                raise InvalidIndexSyntax
+                raise InvalidIndexSyntax("Indexes must be positive integers.")
 
             return slice(first, second)
 
@@ -71,17 +71,15 @@ def get_selected_indexes(string: str) -> Optional[Union[slice, List[int]]]:
             try:
                 indexes = list(map(int, comma_splitted_string))
             except ValueError:
-                raise InvalidIndexSyntax
+                raise InvalidIndexSyntax("Indexes must be integers.")
 
             filtered_indexes: List[int] = []
             for index in indexes:
                 if index < 0:
-                    raise InvalidIndexSyntax
+                    raise InvalidIndexSyntax("Indexes must be positive integers.")
                 if index not in filtered_indexes:
                     filtered_indexes.append(index)
-
             return filtered_indexes
-
-        raise InvalidIndexSyntax
+        raise InvalidIndexSyntax(f"Invalid index syntax: {string}")
 
 
