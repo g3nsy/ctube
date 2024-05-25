8c0b147a9ba47ce99667772d219bf98b51780ddd
g3nsy <g3nsydev@gmail.com>
2024-05-25 12:56:18 +0200

    Removed useless functions and added a new write function to improve code syntax

--- ctube/printers.py
+++ ctube/printers.py
@@ -1,50 +1,26 @@
 import shutil
 from typing import List
 import ctube
-from ctube.cmds import Commands
+from ctube.cmds import Command
 from ctube.colors import color, Color
 from ctube.containers import MusicItem
 
 
 def print_header() -> None:
-    print(color("░█▀▀░▀█▀░█░█░█▀▄░█▀▀░", Color.GREEN))
-    print(color("░█░░░░█░░█░█░█▀▄░█▀▀░", Color.GREEN))
-    print(color("░▀▀▀░░▀░░▀▀▀░▀▀░░▀▀▀░", Color.GREEN))
+    write("░█▀▀░▀█▀░█░█░█▀▄░█▀▀░", Color.GREEN)
+    write("░█░░░░█░░█░█░█▀▄░█▀▀░", Color.GREEN)
+    write("░▀▀▀░░▀░░▀▀▀░▀▀░░▀▀▀░", Color.GREEN)
     print(f"\u2022 {color('version', Color.BOLD)}: {color(ctube.__version__, Color.BLUE)}")
     print("\u2022 source: https://github.com/g3nsy/ctube")
     print("\u2022 Type 'help' to list the available commands")
 
 
-def print_info(cmd_name: str, verbose: bool = False) -> None:
-    try:
-        cmd_obj = Commands.get_by_name(cmd_name)
-        if verbose:
-            print(cmd_obj.value.long_description)
-        else:
-            print(cmd_obj.value.short_description)
-        print(color("Args:", Color.BLUE))
-        if cmd_obj.value.args:
-            for arg in cmd_obj.value.args:
-                print(color(f"{arg.name}:", Color.BOLD), arg.description)
-    except KeyError:
-        print(color(f"Invalid argument for command 'info': {cmd_name}", Color.RED))
-        print(color(f"Valid arguments are {Commands.INFO.value.accepted_args}", Color.RED))
-
-
-def print_help(verbose: bool = False) -> None:
-    print(color(":: Helper", Color.GREEN))
-    for cmd in Commands:
+def print_help() -> None:
+    write(":: Helper", Color.GREEN)
+    for cmd in Command:
         print(f"\u2022 {color(cmd.value.name, Color.BOLD)}:")
-        if verbose:
-            print(color(cmd.value.long_description, Color.BLUE))
-        else:
-            print(color(cmd.value.short_description, Color.BLUE))
+        write(cmd.value.description, Color.GREEN)
         print()
-
-def print_centered(string: str) -> None:
-    terminal_columns = shutil.get_terminal_size().columns
-    spaces = " " * ((terminal_columns // 2) - len(string) // 2)
-    print(f"{spaces}{string}")
 
 
 def print_music_items(music_items: List[MusicItem]) -> None:
@@ -64,3 +40,7 @@ def print_music_items(music_items: List[MusicItem]) -> None:
 
 def clear_screen() -> None:
     print("\033c", end="")
+
+
+def write(string: str, col: Color = Color.WHITE):
+    print(color(string, color=col))
