f35b6045fc57dc5ceade656e5a9be1806d9d4067
g3nsy <g3nsydev@gmail.com>
2024-05-25 13:20:10 +0200

    Remvoed useless errors

--- ctube/errors.py
+++ ctube/errors.py
@@ -3,24 +3,5 @@ class CTubeError(Exception):
        this module inherit from this one."""
 
 
-class InvalidCommandArgument(CTubeError):
-    """It occurs when at least one argument passed as input 
-       to a command is considered invalid."""
-
-
-class CommandNotFoundError(CTubeError):
-    """It occurs when the user provides an unknown command."""
-
-
-class ArtistNotFoundError(CTubeError):
-    """This occurs when, during the process of extracting 
-       an artist's id, the id is not found."""
-
-
-class ContentNotFoundError(CTubeError):
-    """This occurs when, during the process of extracting 
-       an artist's content, the content is not found."""
-
-
 class InvalidIndexSyntax(CTubeError):
     """ """
