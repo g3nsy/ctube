4a50be03e44504219be85bff00925036ea97a172
g3nsy <g3nsydev@gmail.com>
2024-05-29 09:56:34 +0200

    Added exceptions 'NoMP4StreamAvailable and EmptyStreamQuery' to improve error handling in the 'download' module. Added documentation.

--- ctube/errors.py
+++ ctube/errors.py
@@ -4,4 +4,13 @@ class CTubeError(Exception):
 
 
 class InvalidIndexSyntax(CTubeError):
-    """ """
+    """ Occurs when the user provides an argument 
+        to the 'download' command with invalid syntax."""
+
+
+class NoMP4StreamAvailable(CTubeError):
+    """ Occurs when no stream with subtype mp4 is available. """
+
+
+class EmptyStreamQuery(CTubeError):
+    """This occurs when there are no streams for a song."""
