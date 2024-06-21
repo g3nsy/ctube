9f6e783d798efd087adc80b1dfc714b726876312
g3nsy <g3nsydev@gmail.com>
2024-06-21 17:39:18 +0200

    Added filter_albumbs_by_index/by_regex methods

--- ctube/helpers.py
+++ ctube/helpers.py
@@ -1,5 +1,6 @@
+import re
 import requests
-from typing import List , Callable
+from typing import List , Callable, Dict
 from urllib.error import URLError
 from httpx import ReadTimeout, ConnectTimeout, ConnectError
 from innertube.errors import RequestError
@@ -10,7 +11,7 @@ from ctube.colors import Color
 from ctube.printers import write
 
 
-def filter_albums(albums: List[Album], user_input: str) -> List[Album]:
+def filter_albums_by_indexes(albums: List[Album], user_input: str) -> List[Album]:
     selected_indexes = parse_indexes(user_input)
     if not selected_indexes:
         return albums
@@ -37,6 +38,15 @@ def filter_albums(albums: List[Album], user_input: str) -> List[Album]:
                 raise InvalidIndexSyntax(f"Invalid index: {str_incorrect_indexes}")
         else:
             return [albums[index] for index in selected_indexes]
+
+
+def filter_albums_by_regex(albums: List[Album], pattern: str) -> Dict[int, Album]:
+    filtered_albums: Dict[int, Album] = {}
+    for i in range(len(albums)):
+        res = re.search(string=albums[i].title, pattern=pattern)
+        if res:
+            filtered_albums[i] = albums[i]
+    return filtered_albums
 
 
 def handle_connection_errors(func: Callable) -> Callable:
